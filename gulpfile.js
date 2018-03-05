a883252fc15303e3c536417277096bb47a099265
Brad Westfall <brad@azpixels.com>
2018-03-05 00:00:16 -0700

    a good starting point

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const ghPages = require('gulp-gh-pages')
+ 
+gulp.task('sass', () => {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./public/css'))
+})
+ 
+gulp.task('sass:watch', () => {
+  gulp.watch('./scss/**/*.scss', ['sass'])
+})
+
+gulp.task('deploy', () => {
+  return gulp.src('./public/**/*').pipe(ghPages())
+})
+
+gulp.task('default', ['sass', 'sass:watch'])
